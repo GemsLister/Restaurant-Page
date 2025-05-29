a06baad91b7cdbc9933a5b117c91b28e2d022d32
GemsLister <jameslester.l.lopez@gmail.com>
2025-05-29 18:51:35 +0800

    add taglineContainer, taglineText, section, and fast food image

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 import "./style.css";
-import fastFood from "./images/fast-food.jpeg";
+import fastFood from "./images/fast-food.png";
 
 export const mainElement = document.createElement("main");
-
-// first-section create-element
+// first section of the main element
 const firstSection = document.createElement("section");
 firstSection.classList.add("firstSection");
-// fast food image create-element
-const fastFoodImage = document.createElement("img");
-fastFoodImage.src = fastFood;
-fastFoodImage.classList.add("fastFoodImage");
-// tagline create-element
+// tagline
+const taglineContainer = document.createElement("div");
+taglineContainer.classList.add("taglineContainer");
+// tagline text
+const taglineText = document.createElement("p");
+taglineText.classList.add("taglineText");
+taglineText.textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+// tagline h1
 const tagline = document.createElement("h1");
-tagline.textContent = `Mother's tastes the best!`;
+tagline.classList.add("tagline");
+tagline.textContent = 'Welcome to Lutong Bahay ni Nanay!';
+taglineContainer.append(tagline, taglineText);
+// image that displays fast food image
+const fastFoodImageContainer = document.createElement('figure');
+const fastFoodImage =new Image();
+fastFoodImage.classList.add("fastFoodImage");
+fastFoodImage.src = fastFood;
+fastFoodImageContainer.classList.add("fastFoodImageContainer");
+fastFoodImageContainer.appendChild(fastFoodImage);
 
-firstSection.appendChild(tagline);
+firstSection.append(taglineContainer, fastFoodImageContainer);
 mainElement.appendChild(firstSection);
